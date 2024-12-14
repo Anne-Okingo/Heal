e07aa52af94781d91499dbd0323a9913e94b7047
andyosyndoh <andyovvo8@gmail.com>
2024-12-14 15:44:35 +0300

    chore: added routes checker

--- internals/routes/routes.go
+++ internals/routes/routes.go
@@ -1,5 +1,10 @@
 package routes
 
+import (
+	"net/http"
+	"strings"
+)
+
 // Allowed routes
 var allowedRoutes = map[string]bool{
 	"/":                 true,
@@ -12,5 +17,22 @@ var allowedRoutes = map[string]bool{
 	"/api/logout":       true,
 	"/api/gemini":       true,
 	"/api/speechify":    true,
-	"/get-heard": true,
+	"/get-heard":        true,
+}
+
+// RouteChecker is a middleware that checkes allowed routes
+func RouteChecker(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasPrefix(r.URL.Path, "/static/") {
+			// Static(w,r)
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		if _, ok := allowedRoutes[r.URL.Path]; !ok {
+			handlers.NotFoundHandler(w, r)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
