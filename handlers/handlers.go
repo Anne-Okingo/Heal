2e747edb1d412e219e885183aa67799aea50a92a
adaken4 <adakennedy6@gmail.com>
2024-12-14 16:36:18 +0300

    [feat]: adding function that will get the database for updates

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -404,3 +404,36 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		"message": "Login successful",
 	})
 }
+
+func Getdb(name string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", name)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	// Create the users table if it doesn't exist
+	createTableQuery := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		username TEXT UNIQUE NOT NULL,
+		password TEXT NOT NULL
+	);`
+	if _, err := db.Exec(createTableQuery); err != nil {
+		log.Fatalf("Failed to create table: %v", err)
+	}
+
+	createsessionquerry := `
+	CREATE TABLE IF NOT EXISTS sessions (
+		session_id TEXT PRIMARY KEY,
+		user_id INTEGER NOT NULL,
+		expires_at DATETIME NOT NULL,
+		FOREIGN KEY (user_id) REFERENCES users(id)
+	);`
+
+	if _, err := db.Exec(createsessionquerry); err != nil {
+		log.Fatalf("Failed to create table: %v", err)
+	}
+
+	return db, nil
+}
