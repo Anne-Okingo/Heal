f15fe8e1603fd0d0eecb219f35bb01e652b16bd6
hanapiko <apikojuma94@gmail.com>
2024-12-14 17:09:39 +0300

    feat: integrate Spline 3D scene into Home component

---
+++ views/static/index.js
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+    return (
+        <main>
+            <Spline
+                scene="https://prod.spline.design/TVRjoKZK9sBOJPd3/scene.splinecode"
+            />
+        </main>
+    );
+};
